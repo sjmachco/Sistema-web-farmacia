d12d8e6fb8c1c2c5e1314822fd640cf2cf330899
TIVE <tiven_1_2@hotmail.com>
2026-05-06 13:27:51 -0500

    se modificio la conexion a la base de datos.

--- src/main/java/config/Conexion.java
+++ src/main/java/config/Conexion.java
@@ -12,12 +12,12 @@ import java.util.logging.Logger;
  */
 public class Conexion {
     Connection conexion = null;
-    private final String url = System.getProperty("DB_URL_DEPLOY_SUPABASE");
+    private final String url = System.getenv("DB_URL_DEPLOY_SUPABASE");
     //private final String url = "jdbc:mysql://localhost:3306/farmacia";
     private final String driver = "org.postgresql.Driver";
     //private final String driver = "com.mysql.cj.jdbc.Driver";
-    private final String user = System.getProperty("DB_USER_NAME_DEPLOY_SUPABASE");
-    private final String pass = System.getProperty("DB_PASSWORD_DEPLOY_SUPABASE");
+    private final String user = System.getenv("DB_USER_NAME_DEPLOY_SUPABASE");
+    private final String pass = System.getenv("DB_PASSWORD_DEPLOY_SUPABASE");
 
     public Connection getConexion() {
         //Connection cn = null;
